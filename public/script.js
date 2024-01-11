f9cccfb081bb41c1a2e8faf3375386938c5b7ebe
soofka <pansoofka@gmail.com>
2024-01-11 12:40:54 +0100

    modular js app done

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ const app = {
     init: function() {
         this.initLang();
         this.initTheme();
-        initIntroTextSwitcher();
+        this.initIntroTextSwitcher();
     },
     
     initLang: function() {
@@ -24,7 +24,7 @@ const app = {
             this.setLang(navigator.language.substring(0,2));
         });
     
-        document.querySelector('#lang-toggle').addEventListener('click', () => {
+        this.getElement('lang-toggle', '#lang-toggle').addEventListener('click', () => {
             this.setLang(this.lang === 'en' ? 'pl' : 'en');
         });
     },
@@ -81,7 +81,7 @@ const app = {
             this.setTheme(e.matches ? 'dark' : 'light');
         });
     
-        document.querySelector('#theme-toggle').addEventListener('click', () => {
+        this.getElement('theme-toggle', '#theme-toggle').addEventListener('click', () => {
             this.setTheme(this.theme === 'light' ? 'dark' : 'light');
         });
     },
@@ -96,8 +96,10 @@ const app = {
     },
 
     setStyle: function() {
-        const styleLink = document.querySelector('link[id="theme"]');
-        styleLink.href = styleLink.attributes[`data-href-${this.theme}`].value;
+        document.documentElement.style.display = 'none';
+
+        const themeStyleLink = this.getElement('theme-style-link', 'link[id="theme"]');
+        themeStyleLink.href = themeStyleLink.attributes[`data-href-${this.theme}`].value;
     
         // const manifestLink = document.querySelector('link[rel="manifest"]');
         // manifestLink.href = manifestLink.attributes[`data-href-${theme}`].value;
@@ -107,15 +109,93 @@ const app = {
     
         // const themeColorMeta = document.querySelector('meta[name="theme-color"]');
         // themeColorMeta.content = theme === 'dark' ? '#000' : '#fff';
+
+        document.documentElement.style.display = '';
     },
 
-    getElement: function(name, selector) {
-        return this.getElements(name, selector)[0];
+    initIntroTextSwitcher: function() {
+        console.log('updated');
+        const container = this.getElement('intro-text-switcher', '#intro-text-switcher');
+        const texts = container.dataset.texts.split(',');
+        const text = this.getElement('intro-text-switcher-text', '.text', container);
+        const blinker = this.getElement('intro-text-switcher-blinker', '.blinker', container);
+
+        const forwardTimeoutRange = [150, 300];
+        const backwardTimeoutRange = [30, 60];
+        const blinkInterval = 500;
+        const fullWordBlinkCount = 6;
+        const noWordBlinkCount = 2;
+        
+        let timeout = 0;
+        let currentTextId = 0;
+        let currentLetterId = 0;
+        let currentBlinkCount = 0;
+        let mode = 0; // 0 - forward, 1 - blink when typed, 2 - backward, 3 - blink when empty
+    
+        const type = () => {
+            text.innerText = texts[currentTextId].substring(0, currentLetterId);
+    
+            switch (mode) {
+                case 0:
+                    currentLetterId++;
+                    timeout = getTimeout(forwardTimeoutRange);
+                    if (currentLetterId > texts[currentTextId].length) {
+                        mode = 1;
+                    }
+                    break;
+    
+                case 1:
+                    blink();
+                    currentBlinkCount++;
+                    timeout = blinkInterval;
+                    if (currentBlinkCount >= fullWordBlinkCount) {
+                        currentBlinkCount = 0;
+                        mode = 2;
+                    }
+                    break;
+    
+                case 2:
+                    currentLetterId--;
+                    timeout = getTimeout(backwardTimeoutRange);
+                    if (currentLetterId === 0) {
+                        mode = 3;
+                        currentTextId++;
+                        if (currentTextId >= texts.length) {
+                            currentTextId = 0;
+                        }
+                    }
+                    break;
+    
+                case 3:
+                    blink();
+                    currentBlinkCount++;
+                    timeout = blinkInterval;
+                    if (currentBlinkCount >= noWordBlinkCount) {
+                        currentBlinkCount = 0;
+                        mode = 0;
+                    }
+                    break;
+    
+                default:
+                    break;
+            }
+    
+            setTimeout(type, timeout);
+        }
+    
+        const getTimeout = (range) => Math.random() * (range[1] - range[0]) + range[0];
+        const blink = () => blinker.style.visibility === 'hidden' ? blinker.style.visibility = '' : blinker.style.visibility = 'hidden';
+    
+        type();
     },
 
-    getElements: function(name, selector) {
+    getElement: function(name, selector, parent = document) {
+        return this.getElements(name, selector, parent)[0];
+    },
+
+    getElements: function(name, selector, parent = document) {
         if (!Object.hasOwnProperty(this.elements, name)) {
-            this.elements[name] = document.querySelectorAll(selector);
+            this.elements[name] = parent.querySelectorAll(selector);
         }
         return this.elements[name];
     },
@@ -123,104 +203,3 @@ const app = {
 };
 
 app.init();
-
-// let lang = 'en';
-// let theme = 'light';
-// let labels = {};
-
-// const setLabels = async () => {
-// }
-
-// const initTheme = () => {
-// }
-
-// const setTheme = () => {
-//     const styleLink = document.querySelector('link[id="theme"]');
-//     styleLink.href = styleLink.attributes[`data-href-${theme}`].value;
-
-//     // const manifestLink = document.querySelector('link[rel="manifest"]');
-//     // manifestLink.href = manifestLink.attributes[`data-href-${theme}`].value;
-
-//     // const faviconLink = document.querySelectorAll('link[rel="icon"]');
-//     // faviconLink.href = faviconLink.attributes[`data-href-${theme}`].value;
-
-//     // const themeColorMeta = document.querySelector('meta[name="theme-color"]');
-//     // themeColorMeta.content = theme === 'dark' ? '#000' : '#fff';
-
-//     localStorage.setItem('theme', theme);
-//     document.querySelector('#theme-toggle').innerText = theme === 'dark' ? 'light' : 'dark';
-// }
-
-function initIntroTextSwitcher() {
-    const textContainer = document.querySelector('#intro h3 .text');
-    const blinker = document.querySelector('#intro h3 .blinker');
-    const texts = ['developer', 'architect', 'teacher'];
-
-    const forwardTimeoutRange = [150, 300];
-    const backwardTimeoutRange = [30, 60];
-    const blinkInterval = 500;
-    const fullWordBlinkCount = 6;
-    const noWordBlinkCount = 2;
-    
-    let timeout = 0;
-    let currentTextId = 0;
-    let currentLetterId = 0;
-    let currentBlinkCount = 0;
-    let mode = 0; // 0 - forward, 1 - blink when typed, 2 - backward, 3 - blink when empty
-
-    const type = () => {
-        textContainer.innerText = texts[currentTextId].substr(0, currentLetterId);
-
-        switch (mode) {
-            case 0:
-                currentLetterId++;
-                timeout = getTimeout(forwardTimeoutRange);
-                if (currentLetterId > texts[currentTextId].length) {
-                    mode = 1;
-                }
-                break;
-
-            case 1:
-                blink();
-                currentBlinkCount++;
-                timeout = blinkInterval;
-                if (currentBlinkCount >= fullWordBlinkCount) {
-                    currentBlinkCount = 0;
-                    mode = 2;
-                }
-                break;
-
-            case 2:
-                currentLetterId--;
-                timeout = getTimeout(backwardTimeoutRange);
-                if (currentLetterId === 0) {
-                    mode = 3;
-                    currentTextId++;
-                    if (currentTextId >= texts.length) {
-                        currentTextId = 0;
-                    }
-                }
-                break;
-
-            case 3:
-                blink();
-                currentBlinkCount++;
-                timeout = blinkInterval;
-                if (currentBlinkCount >= noWordBlinkCount) {
-                    currentBlinkCount = 0;
-                    mode = 0;
-                }
-                break;
-
-            default:
-                break;
-        }
-
-        setTimeout(type, timeout);
-    }
-
-    const getTimeout = (range) => Math.random() * (range[1] - range[0]) + range[0];
-    const blink = () => blinker.style.visibility === 'hidden' ? blinker.style.visibility = '' : blinker.style.visibility = 'hidden';
-
-    type();
-}
