1352f346f97c74a1171308798941b71c5f03187d
soofka <pansoofka@gmail.com>
2024-01-15 18:03:10 +0100

    started collecting build output as it goes in order to pass it to subsequent functions

--- builder/build.js
+++ builder/build.js
@@ -9,15 +9,17 @@ export const build = (config) => builder.init(config);
 const builder = {
     
     config: {},
-    files: {
-        html: [],
-        css: [],
-        js: [],
-        json: [],
-        img: [],
-        other: [],
+    srcFiles: {},
+    distFiles: {},
+    templateFiles: {},
+    filesGroupMap: {
+        html: ['.html'],
+        css: ['.css'],
+        js: ['.js'],
+        json: ['.json', '.webmanifest'],
+        img: ['.png', '.jpg', '.jpeg'],
     },
-    imgMap: {},
+
 
     init: function(config) {
         // include config validation/default
@@ -29,9 +31,8 @@ const builder = {
         await this.initDistDir();
         await this.initTemplates();
         await this.initSrcFiles();
-        console.log(this.files);
         await this.parseImgFiles();
-        console.log(this.imgMap);
+        console.log('gonna emit', this.distFiles);
         await this.parseHtmlFiles();
         // await this.parseCssFiles();
         // await this.parseJsFiles();
@@ -51,61 +52,46 @@ const builder = {
         await fs.mkdir(this.config.dist, { recursive: true });
     },
 
-    initSrcFiles: async function() {
-        const dir = await readDirRec(this.config.src);
-        this.files = await groupDirByFileTypeRec(dir, {
-            html: ['.html'],
-            css: ['.css'],
-            js: ['.js'],
-            json: ['.json', '.webmanifest'],
-            img: ['.png', '.jpg', '.jpeg'],
-        });
-    },
-
     initTemplates: async function() {
         const dir = await readDirRec(this.config.templates);
-        for (let template of dir.content) {
+        await this.runTemplates(dir.content);
+        this.templateFiles = dir.content; //groupDirByFileTypeRec(dir, this.filesGroupMap);
+    },
+
+    runTemplates: async function(templates) {
+        for (let template of templates) {
             // is is cross-env?
-            const modulePath = path.join('file:///', template.absPath, `${template.name}${template.type}`);
+            const modulePath = path.join('file:///', template.full);
             const module = await import(modulePath);
             const moduleDefault = module.default;
 
             if (typeof moduleDefault === 'function') {
                 const resultContent = moduleDefault(this.config.data);
-                const resultPath = path.join(this.config.templatesOutput, template.relPath, template.name);
-                try {
-                    await fs.mkdir(path.dirname(resultPath), { recursive: true });
-                } catch(e) {
-                    if (e !== 'EEXIST') {
-                        throw e;
-                    }
-                }
-                await fs.writeFile(resultPath, resultContent);
+                const resultPath = path.join(this.config.templatesOutput, template.rel, template.name);
+                await writeFile(resultPath, resultContent);
             } else if (typeof moduleDefault === 'object') {
                 for (let key of Object.keys(moduleDefault)) {
                     const resultContent = moduleDefault[key](this.config.data);
                     const dotIndex = template.name.lastIndexOf('.');
                     const resultName = `${template.name.substring(0, dotIndex)}${key}${template.name.substring(dotIndex)}`;
-                    const resultPath = path.join(this.config.templatesOutput, template.relPath, resultName);
-                    try {
-                        await fs.mkdir(path.dirname(resultPath), { recursive: true });
-                    } catch(e) {
-                        if (e !== 'EEXIST') {
-                            throw e;
-                        }
-                    }
-                    await fs.writeFile(resultPath, resultContent);
+                    const resultPath = path.join(this.config.templatesOutput, template.rel, resultName);
+                    await writeFile(resultPath, resultContent);
                 }
             }
         }
     },
 
+    initSrcFiles: async function() {
+        const dir = await readDirRec(this.config.src);
+        this.srcFiles = await groupDirByFileTypeRec(dir, this.filesGroupMap);
+        this.distFiles = {};
+    },
+
     parseImgFiles: async function() {
         const CURRENT = 'CURRENT';
+        this.distFiles.img = [];
 
-        for (let file of this.files.img) {
-            const srcFileRelPath = path.join(file.relPath, `${file.name}${file.type}`);
-
+        for (let file of this.srcFiles.img) {
             let imageWidths = [CURRENT];
             if (this.config.options.resizeImages) {
                 imageWidths = this.config.options.resizedImagesWidths;
@@ -115,19 +101,15 @@ const builder = {
             if (this.config.options.optimizeImages) {
                 imageTypes = this.config.options.optimizedImagesTypes;
             }
-            
-            const imgWidthVersions = [];
         
             for (let width of imageWidths) {
                 const isCurrentWidth = width === CURRENT;
                 const distFileName = isCurrentWidth ? file.name : `${file.name}-${width}`;
 
-                const imgTypeVersions = {};
-
                 for (let type of imageTypes) {
                     const isCurrentType = type === CURRENT;
-                    const distFileFullName = isCurrentType ? `${distFileName}${file.type}` : `${distFileName}.${type}`;
-                    const distFileRelPath = path.join(file.relPath, distFileFullName);
+                    const distFileFullName = isCurrentType ? `${distFileName}${file.ext}` : `${distFileName}.${type}`;
+                    const distFileAbsPath = this.getFileDistAbsPath(file, distFileFullName);
 
                     let parseFunction;
                     if (isCurrentWidth && isCurrentType) {
@@ -140,75 +122,63 @@ const builder = {
                         parseFunction = async (srcPath) => await sharp(srcPath).resize(width)[type]().toBuffer();
                     }
 
-                    await this.parseFile(
-                        this.getFileSrcAbsPath(file),
-                        this.getFileDistAbsPath(file, distFileFullName),
-                        parseFunction,
-                    );
-
-                    imgTypeVersions[type] = distFileRelPath;
+                    await this.parseFile(file.full, distFileAbsPath, parseFunction);
+                    this.distFiles.img.push(createFileObject(distFileAbsPath, file.rel));
                 }
-    
-                imgWidthVersions.push(imgTypeVersions);
             }
-    
-            this.imgMap[srcFileRelPath] = imgWidthVersions;
         }
     },
 
     parseHtmlFiles: async function () {
-        for (let file of this.files.html) {
+        for (let file of this.srcFiles.html) {
             // substitute variables from config
             // replace imgs with responsive
             // prepopulate labels
             // set defaults to styling
-            const html = await readFile(this.getFileSrcAbsPath(file), { encoding: 'utf8' });
+            const html = await readFile(file.full, { encoding: 'utf8' });
             const qs = cheerio.load(html);
 
-            for (let img of qs('img')) {
-                for (let attr of img.attributes) {
-                    if (attr.name === 'src' && Object.keys(this.imgMap).includes(attr.value)) {
-                        const imgObject = this.imgMap[attr.value];
-                        const imgElement = qs(img);
-                        imgElement.replaceWith(() => {
-                            let picture = '<picture>';
-                            for (let type of this.config.options.optimizedImagesTypes) {
-                                picture += '<source srcet="';
-                                for (let widthIndex in this.config.options.resizedImagesWidths) {
-                                    picture += `${imgObject[widthIndex][type]} `;
-                                    picture += `${this.config.options.resizedImagesWidths[widthIndex]}w`;
-                                    if (widthIndex < this.config.options.resizedImagesWidths.length - 1) {
-                                        picture += ', ';
-                                    }
-                                }
-                                picture += `" type="image/${type}">`
-                            }
-                            picture += '</picture>';
-                            return picture;
-                        });
-                        //         <picture>
-                        //         ${this.config.options.optimizedImagesTypes.map((type) => `
-                        //             <source
-                        //                 srcset="${this.config.options.resizedImagesWidths.map((width, index) => `
-                        //                     ${imgObject[index][type]} ${width}w
-                        //                 `)}"
-                        //                 type="image/${type}"
-                        //             >
-                        //         `)}
-                        //         <!-- img fallback to be added -->
-                        //     </picture>
-                        // `);
-                    }
-                }
-                // console.log('image from', file.name, img.attributes);
-            }
+            // for (let img of qs('img')) {
+            //     for (let attr of img.attributes) {
+            //         if (attr.name === 'src' && Object.keys(this.imgMap).includes(attr.value)) {
+            //             const imgObject = this.imgMap[attr.value];
+            //             const imgElement = qs(img);
+            //             imgElement.replaceWith(() => {
+            //                 let picture = '<picture>';
+            //                 for (let type of this.config.options.optimizedImagesTypes) {
+            //                     picture += '<source srcet="';
+            //                     for (let widthIndex in this.config.options.resizedImagesWidths) {
+            //                         picture += `${imgObject[widthIndex][type]} `;
+            //                         picture += `${this.config.options.resizedImagesWidths[widthIndex]}w`;
+            //                         if (widthIndex < this.config.options.resizedImagesWidths.length - 1) {
+            //                             picture += ', ';
+            //                         }
+            //                     }
+            //                     picture += `" type="image/${type}">`
+            //                 }
+            //                 picture += '</picture>';
+            //                 return picture;
+            //             });
+            //             //         <picture>
+            //             //         ${this.config.options.optimizedImagesTypes.map((type) => `
+            //             //             <source
+            //             //                 srcset="${this.config.options.resizedImagesWidths.map((width, index) => `
+            //             //                     ${imgObject[index][type]} ${width}w
+            //             //                 `)}"
+            //             //                 type="image/${type}"
+            //             //             >
+            //             //         `)}
+            //             //         <!-- img fallback to be added -->
+            //             //     </picture>
+            //             // `);
+            //         }
+            //     }
+            //     // console.log('image from', file.name, img.attributes);
+            // }
 
             // qs('title').text('PARSOWANY ' + file.name);
 
-            await writeFile(
-                this.getFileDistAbsPath(file),
-                htmlMinify(qs.html(), this.config.options.minify.html),
-            );
+            await writeFile(this.getFileDistAbsPath(file), htmlMinify(qs.html(), this.config.options.minify.html));
         }
     },
     
@@ -260,27 +230,23 @@ const builder = {
         return parseFile(srcAbsPath, distAbsPath, parseFunction, fs.copyFile);
     },
 
-    getFileSrcAbsPath: function(file) {
-        return path.join(file.absPath, `${file.name}${file.type}`);
-    },
-
     getFileDistAbsPath: function(file, newFullName = undefined) {
-        return path.join(this.config.dist, file.relPath, newFullName ? newFullName : `${file.name}${file.type}`);
+        return path.join(this.config.dist, file.rel, newFullName ? newFullName : file.base);
     },
 
 }
 
-async function groupDirByFileTypeRec(dir, fileTypeGroups, includeOther = true, fileGroups = {}) {
+function groupDirByFileTypeRec(dir, fileTypeGroups, includeOther = true, fileGroups = {}) {
     for (let item of dir.content) {
         if (item.isDir) {
-            fileGroups = await groupDirByFileTypeRec(item, fileTypeGroups, includeOther, fileGroups);
+            fileGroups = groupDirByFileTypeRec(item, fileTypeGroups, includeOther, fileGroups);
         } else {
             let matched = false;
             for (let group of Object.keys(fileTypeGroups)) {
                 if (!fileGroups.hasOwnProperty(group)) {
                     fileGroups[group] = [];
                 }
-                if (fileTypeGroups[group].includes(item.type)) {
+                if (fileTypeGroups[group].includes(item.ext)) {
                     matched = true;
                     fileGroups[group].push(item);
                 }
@@ -300,29 +266,33 @@ async function groupDirByFileTypeRec(dir, fileTypeGroups, includeOther = true, f
 async function readDirRec(rootDirAbsPath, currentDirRelPath = '') {
     const dirPath = path.join(rootDirAbsPath, currentDirRelPath);
     const dir = await fs.readdir(dirPath, { withFileTypes: true });
-    const dirMap = {
-        isDir: true,
-        name: path.basename(dirPath),
-        absPath: path.dirname(dirPath),
-        relPath: currentDirRelPath,
-        content: [],
-    };
+    const dirMap = createFileObject(dirPath, currentDirRelPath, true);
 
     for (let item of dir) {
         if (item.isFile()) {
-            const ext = path.extname(item.name);
-            dirMap.content.push({
-                absPath: dirPath,
-                relPath: currentDirRelPath,
-                name: path.basename(item.name, ext),
-                type: ext,
-            });
+            dirMap.content.push(createFileObject(path.join(item.path, item.name), currentDirRelPath));
         } else if (item.isDirectory()) {
             dirMap.content.push(await readDirRec(rootDirAbsPath, path.join(currentDirRelPath, item.name)));
         }
     }
 
     return dirMap;
+}
+
+
+function createFileObject (fullPath, relPath, isDir = false) {
+    const fileObject = {
+        ...path.parse(fullPath),
+        full: fullPath,
+        rel: relPath,
+    };
+
+    if (isDir) {
+        fileObject.isDir = true;
+        fileObject.content = [];
+    }
+
+    return fileObject;
 }
 
 async function readFile(srcPath, options = {}) {
---
+++ img.partial.js
@@ -0,0 +1,5 @@
+const img = () => {
+
+}
+
+export default img;
